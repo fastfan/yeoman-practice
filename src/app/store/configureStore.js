fdfe7fd228564002299994575f6ec47cca4d1054
fanwei <fanwei@citycloud.com.cn>
2023-05-05 14:53:19 +0800

    fix(*): *

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,14 +1,42 @@
-import {createStore} from 'redux';
-import rootReducer from '../reducers/index';
+// import {createStore} from 'redux';
+// import rootReducer from '../reducers/index';
+
+// export default function configureStore(initialState) {
+//   const store = createStore(rootReducer, initialState);
+//   if (module.hot) {
+//     // Enable Webpack hot module replacement for reducers
+//     module.hot.accept('../reducers', () => {
+//       const nextReducer = rootReducer;
+//       store.replaceReducer(nextReducer);
+//     });
+//   }
+//   return store;
+// }
+import {compose, createStore} from 'redux';
+import rootReducer from '../reducers';
+
+import persistState, {mergePersistedState} from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const reducer = compose(
+    mergePersistedState()
+  )(rootReducer, initialState);
+
+  const storage = adapter(window.localStorage);
+
+  const createPersistentStore = compose(
+    persistState(storage, 'state')
+  )(createStore);
+
+  const store = createPersistentStore(reducer);
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextReducer = rootReducer;
+      const nextReducer = require('../reducers').default;
       store.replaceReducer(nextReducer);
     });
   }
+
   return store;
 }
